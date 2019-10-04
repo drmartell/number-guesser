b36499f569fd8166d5a17e3b0d017ab1d38052c7
503-888-1044 Martell <drmartell@gmail.com>
2019-10-04 11:51:02 -0700

    additional refactoring

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import { compareNumbers } from './functions.js';
 
 let tooHigh = '';
 let tooLow = '';
-let thisCell;
-
 const triesRemaining = document.getElementById('tries-remaining');
 const numTable = document.getElementById('numbers-table');
 const rawUserGuess = document.getElementById('user-guess-id');
@@ -12,18 +10,19 @@ const playAgainButton = document.getElementById('play-again');
 const notifier = document.getElementById('notifier');
 const tooHighSpan = document.getElementById('too-high');
 const tooLowSpan = document.getElementById('too-low');
-
-const generateRandom20 = () => Math.floor(Math.random() * Math.floor(20)) + 1;
-const secretNumber = generateRandom20();
+const secretNumber = Math.ceil(Math.random() * 20);
 
 triesRemaining.innerText = 4;
 
 submitButton.addEventListener('click', () => updateTable(rawUserGuess.value));
-
 playAgainButton.addEventListener('click', () => resetGame());
 
-function updateTable(numGuessed) {
-    document.getElementById('num' + numGuessed).click();
+if (numTable !== null) {
+    for (let i = 0; i < numTable.rows.length; i++) {
+        for (let j = 0; j < numTable.rows[i].cells.length; j++) {
+            document.getElementById(numTable.rows[i].cells[j].id).addEventListener('click', (event) => userState(event.target));
+        }
+    }
 }
 
 function resetGame() {
@@ -31,17 +30,9 @@ function resetGame() {
     window.location.reload();
 }
 
-if (numTable !== null) {
-    for (let i = 0; i < numTable.rows.length; i++) {
-        for (let j = 0; j < numTable.rows[i].cells.length; j++) {
-            thisCell = numTable.rows[i].cells[j].id;
-            document.getElementById(numTable.rows[i].cells[j].id).addEventListener('click', () => userState(thisCell));
-        }
-    }
-}
 function userState(thisCell) {
     breakable: if (triesRemaining.innerText > 0) {
-        if (thisCell.className === 'chosen') break breakable
+        if (thisCell.className === 'chosen') break breakable;
         thisCell.className = 'chosen';
         triesRemaining.innerText -= 1;
         let numUserClicked = thisCell.innerText;
@@ -50,26 +41,30 @@ function userState(thisCell) {
         } else if (compareNumbers(numUserClicked, secretNumber) === -1) {
             updateTooLow(numUserClicked);
         } else {
-            doWin()
+            doWin(playAgainButton, thisCell);
         }
     }
     if (parseInt(triesRemaining.innerText) === 0) {
-        doLoss()
+        doLoss(playAgainButton);
     }
-}    
-    
-function doLoss() {
+} 
+
+function updateTable(numGuessed) {
+    document.getElementById('num' + numGuessed).click();
+}
+   
+function doLoss(playButton) {
     notifier.innerText = `SO sorry, ya LOST - it was ${secretNumber}!`;
     notifier.className = 'loser-text';
-    playAgainButton.className = 'visible';
-};
+    playButton.className = 'visible';
+}
     
-function doWin() {
+function doWin(playButton, clickedCell) {
     notifier.innerText = `OMG, you WON on your ${(4 - triesRemaining.innerText) === 4 ? '4th' :  (4 - triesRemaining.innerText) === 3 ? '3rd' : (4 - triesRemaining.innerText) === 2 ? '2nd' : '1st'} try!`; triesRemaining.innerText = -1;
     notifier.className = 'winner-text';
-    this.className = 'winner';
-    playAgainButton.className = 'visible';
-};
+    clickedCell.className = 'winner';
+    playButton.className = 'visible';
+}
 
 function updateTooLow(userSelection) {
     tooLow += tooLow.length > 0 ? ' and ' + userSelection : userSelection;
